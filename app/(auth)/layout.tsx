80d4696b0fb6aa4405e588fa3af1bf011158bdcb
Adityanaik4545 <adityank23@outlook.com>
2025-08-08 23:59:06 +0530

    worked on authentication layout

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Image from 'next/image'
+
+
+const layout = ({children}:{children: React.ReactNode}) => {
+  return (
+    <div className='flex min-h-screen'>
+      <section className='bg-brand p-10'>
+        <div>
+          <Image src="/assets/icons/logo-full.svg" alt="logo" width={224} height={82} className='h-auto'/>
+          <div className='space-y-5 text-white'>
+            <h1 className='h1'>Manage your files the best way</h1>
+            <p className='body-1'>
+              This is the place where you can manage your all documents
+            </p>
+          </div>
+          <Image src="/assets/images/files.png" width={342} height={342} alt='file' className='transition-all hover:rotate-2 hover:scale-105'/>
+        </div>
+      </section>
+      {children}
+    </div>
+  )
+}
+
+export default layout
