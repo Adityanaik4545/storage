cea568b6e3c69728a8c47299024a376a4148a3a5
Adityanaik4545 <adityank23@outlook.com>
2025-08-09 23:56:05 +0530

    Integrate shadcn UI components and build sign-in & sign-up forms

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -2,22 +2,28 @@ import React from 'react'
 import Image from 'next/image'
 
 
-const layout = ({children}:{children: React.ReactNode}) => {
+const layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className='flex min-h-screen'>
-      <section className='bg-brand p-10'>
-        <div>
-          <Image src="/assets/icons/logo-full.svg" alt="logo" width={224} height={82} className='h-auto'/>
+      <section className='bg-brand p-10 hidden w-1/2 items-center justify-center lg:flex xl:w-2/5'>
+        <div className='flex flex-col max-h-[800px] max-w-[430px] justify-center space-y-12'>
+          <Image src="/assets/icons/logo-full.svg" alt="logo" width={224} height={82} className='h-auto' />
           <div className='space-y-5 text-white'>
             <h1 className='h1'>Manage your files the best way</h1>
             <p className='body-1'>
-              This is the place where you can manage your all documents
+              This is the place where you can manage your all documents.
             </p>
           </div>
-          <Image src="/assets/images/files.png" width={342} height={342} alt='file' className='transition-all hover:rotate-2 hover:scale-105'/>
+          <Image src="/assets/images/files.png" width={342} height={342} alt='file' className='transition-all hover:rotate-2 hover:scale-105' />
         </div>
       </section>
-      {children}
+
+      <section className='flex flex-1 flex-col items-center bg-white p-4 py-10 lg:justify-center'>
+        <div className='mb-16 lg:hidden'>
+          <Image src="/assets/icons/logo-full-brand.svg" alt='logo' width={224} height={822} className='h-auto w-[200px] lg:w-[250px]' />
+        </div>
+        {children}
+      </section>
     </div>
   )
 }
