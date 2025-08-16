e19a7588327e05a19aec39ff1c64dee01cd7cc92
Adityanaik4545 <adityank23@outlook.com>
2025-08-16 19:25:10 +0530

    worked on mobile navigation part

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -12,7 +12,7 @@ const Layout = async({children}:{children:React.ReactNode}) => {
     <main className='flex h-screen'>
         <Sidebar {...currentUser} />
         <section className='flex flex-1 h-full flex-col'>
-        <MobileNavigation/> <Header/>
+        <MobileNavigation {...currentUser}/> <Header/>
         <div className='main-content'>
             {children}
         </div>
--- components/MobileNavigation.tsx
+++ components/MobileNavigation.tsx
@@ -1,10 +1,74 @@
+"use client"
 import React from 'react'
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
+import Image from 'next/image'
+import { Separator } from './ui/separator'
+import { usePathname } from 'next/navigation'
+import Link from 'next/link'
+import { cn } from '@/lib/utils'
+import { navItems } from '@/constants/constant'
+import FileUpload from './FileUpload'
 
-const MobileNavigation = () => {
+interface Props{
+  fullName:string,
+  avatar:string,
+  email:string,
+  ownerId:string,
+  accountId:string,
+}
+
+const MobileNavigation = ({fullName, avatar, email, ownerId, accountId}:Props) => {
+  const pathName=usePathname()
   return (
-    <div>
-      Mobile navigation
-    </div>
+    <header className='mobile-header'>
+      <Image src="assets/icons/logo-full-brand.svg" alt='logo' width={120} height={52} className='h-auto' />
+      <Sheet>
+  <SheetTrigger>
+    <Image src='assets/icons/menu.svg' alt='search' width={30} height={30} />
+  </SheetTrigger>
+  <SheetContent className='shad-sheet h-screen px-3'>
+      <SheetTitle>
+        <div className='header-user'>
+        <Image src={avatar} alt='avatar' width={44} height={44} className='header-user-avatar'/>
+        <div className=' sm:hidden lg:block '>
+        <p className='subtitle-2 capitalize'>{fullName}</p>
+        <p className='caption'>{email}</p>
+        </div>
+        </div>
+        <Separator className='mb-4 bg-light-200/20'/>
+      </SheetTitle>
+
+      <nav className='mobile-nav'>
+      <ul className='mobile-nav-list'>
+            {navItems.map(({url, name, icon})=>(
+             <Link key={name} href={url} className='lg:w-full '>
+              <li className={cn("mobile-nav-item", pathName===url && 'shad-active')}>
+                <Image src={icon} width={24} height={24} alt={name} className={cn('nav-icon', pathName===url && 'nav-icon-active')} />
+                <p>{name}</p>
+              </li>
+             </Link>
+      ))}
+
+      </ul>
+      </nav>
+        <Separator className='mb-5 bg-light-200/20'/>
+        <div className='flex flex-col justify-between gap-5 pb-5'>
+        <FileUpload/>
+                  <button type='submit' className='mobile-sign-out-button' onClick={()=>{}}>
+                    <Image src="assets/icons/logout.svg" width={24} height={24} alt='logout' />
+                    <p>Logout</p>
+                  </button>
+        </div>
+  </SheetContent>
+</Sheet>
+    </header>
   )
 }
 
