1dd89be828b8c2462f742d7c0df09f95551e38b2
Adityanaik4545 <adityank23@outlook.com>
2025-08-17 19:10:28 +0530

    worked on file uploading, implemented drag n drop

--- components/FileUpload.tsx
+++ components/FileUpload.tsx
@@ -1,9 +1,66 @@
-import React from 'react'
+"use client"
+import { cn, convertFileToUrl, getFileType } from '@/lib/utils'
+import Image from 'next/image'
+import React, {useCallback, useState} from 'react'
+import {useDropzone} from 'react-dropzone'
+import { file } from 'zod'
+import Thumbnail from './Thumbnail'
 
-const FileUpload = () => {
+interface Props{
+  ownerId:string,
+  accountId:string,
+  className?:string,
+}
+
+const FileUpload = ({ownerId, accountId, className }:Props) => {
+  const [files, setFiles] = useState<File[]>([]);
+
+  const onDrop = useCallback(async(acceptedFiles:File[]) => {
+    setFiles(acceptedFiles)
+  }, [])
+  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+
+  const handleRemoveFile=(e:React.MouseEvent<HTMLImageElement, MouseEvent>, fileName:string)=>{
+      e.stopPropagation();
+      setFiles((prevFiles)=>prevFiles.filter((file)=>file.name !== fileName));
+  }
+
   return (
-    <div>
-      FileUpload
+    <div {...getRootProps()} className='cursor-pointer'>
+      <input {...getInputProps()} />
+      <button type='button' className={cn("uploader-button", className)} >
+        <Image src="/assets/icons/upload.svg" alt='upload' width={24} height={24} />
+        <p>Upload</p>
+      </button>
+      {files.length > 0 && (
+        <ul className='uploader-preview-list'>
+          <h4 className='h4 text-light-100'>Uploading</h4>
+          {files.map((file, index)=>{
+            const {type, extension} = getFileType(file.name);
+            return (
+              <li key={`${file.name}-${index}`} className='uploader-preview-item' >
+                <div className='flex items-center gap-3'>
+                <Thumbnail
+                type={type}
+                extension={extension}
+                url={convertFileToUrl(file)}
+                />
+                <div className='preview-item-name'>
+                  {file.name}
+                  <Image src="/assets/icons/file-loader.gif" alt='loader' height={26} width={80} />
+                </div>
+                </div>
+                <Image src="/assets/icons/remove.svg" alt='remove' height={24} width={24} onClick={(e)=>handleRemoveFile(e,file.name)} />
+              </li>
+            )
+          })}
+        </ul>
+      )}
+      {
+        isDragActive ?
+          <p>Drop the files here ...</p> :
+          <p>Drag 'n' drop some files here, or click to select files</p>
+      }
     </div>
   )
 }
